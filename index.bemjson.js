d29b2b7e05df5c6117cb16b5ec0f7e5ff3a4185d
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-16 22:41:54 +0400

    Link SC sdk.js to the page

--- index.bemjson.js
+++ index.bemjson.js
@@ -6,6 +6,7 @@
         { elem: 'css', url: '_index.css'},
         { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
         { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: 'http://connect.soundcloud.com/sdk.js' },
         { elem: 'js', url: 'index.bemhtml.js' },
         { elem: 'js', url: 'index.js' }
     ],
