4f315c984306cb7f3ac78beaeb66b869c17c5f16
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-16 21:05:18 +0400

    Use client templates to build inner blocks

--- index.bemjson.js
+++ index.bemjson.js
@@ -5,9 +5,8 @@
         { elem: 'css', url: '_index.css'},
         { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
         { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: 'index.bemhtml.js' },
         { elem: 'js', url: 'index.js' }
     ],
-    content: [
-        'Hello, world!'
-    ]
+    content: ''
 })
