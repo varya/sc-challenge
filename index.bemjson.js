c4417992006383b7cee09ad2be3d62c88cb4fc39
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-16 20:50:32 +0400

    Source data of the only page

---
+++ index.bemjson.js
@@ -0,0 +1,13 @@
+({
+    block: 'b-page',
+    title: 'Awesome playlists of SoundCloud',
+    head: [
+        { elem: 'css', url: '_index.css'},
+        { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
+        { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: 'index.js' }
+    ],
+    content: [
+        'Hello, world!'
+    ]
+})
