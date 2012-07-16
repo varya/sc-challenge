fb38bc6ffde81717f3cf5fbaf090ca1d0577d475
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-16 22:42:42 +0400

    First attempt to find tracks with SC.get

--- blocks/b-search/b-search.js
+++ blocks/b-search/b-search.js
@@ -11,12 +11,21 @@ BEM.DOM.decl('b-search', {
     doSearch: function(e) {
         // A method to search
         e.preventDefault();
+        SC.get('/tracks', { genres: 'punk', bpm: { from: 120 } }, function(tracks) {
+          console.log(tracks);
+        });
         console.log('I am searching', this.val());
     }
 
 }, {
 
     live : function() {
+
+        SC.initialize({
+            client_id: '2ffbaf9479281e4b80bd1e929162dcea',
+            redirect_uri: 'http://sc.toivonen.veged.dev.yandex.ru/index.html'
+        });
+
         this.liveInitOnEvent('submit')
             .liveBindTo('submit', function(e){
                 this.doSearch(e);
