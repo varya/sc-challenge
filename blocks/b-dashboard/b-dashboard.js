87290f04fa47ec2a728244d70388e4bb31116d7d
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-17 10:46:59 +0400

    To do wth searching

---
+++ blocks/b-dashboard/b-dashboard.js
@@ -0,0 +1,36 @@
+/** @requires BEM.DOM */
+
+(function() {
+
+BEM.DOM.decl('b-dashboard', {
+
+    onSetMod : {
+        'js' : function() {
+            var bDashboard = this;
+            this.findBlockInside('b-search').on('searched', function(e, data){
+                bDashboard
+                    .clearTracks()
+                    .appendTracks(data);
+            })
+        }
+    },
+    clearTracks: function() {
+        $.each(this.findBlocksInside('searchfield', 'b-serp-item'), function(i, item) {
+            item.remove();
+        })
+        return this;
+    },
+    appendTracks: function(tracks) {
+        var html = '';
+        $.each(tracks, function(i, track){
+            html += BEM.blocks['b-serp-item'].buildFromSearchResult(track);
+        })
+        this.elem('searchfield').append(html);
+        BEM.DOM.init(this.elem('searchfield'));
+
+        return this;
+    }
+
+}, {});
+
+})();
