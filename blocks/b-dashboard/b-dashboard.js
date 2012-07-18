12d0b40be15b36085e73109413f4a1e3a07b6e54
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-18 13:59:14 +0400

    Some comments

--- blocks/b-dashboard/b-dashboard.js
+++ blocks/b-dashboard/b-dashboard.js
@@ -2,28 +2,36 @@
 
 (function() {
 
+/* Block-container for the application */
 BEM.DOM.decl('b-dashboard', {
 
     onSetMod : {
+
         'js' : function() {
+
             var bDashboard = this;
 
+            /* Adding selected track to a current playlist */
             BEM.blocks['b-serp-item'].on('selected', function(e){
                 BEM.blocks['b-playlist'].getCurrent().add(e.block.track());
             })
 
+            /* Appening a new playlist when it's born */
             BEM.blocks['b-playlist'].on('birth', function(e, data){
                 BEM.DOM.append(bDashboard.elem('playlists'), data.html);
             })
 
         }
+
     },
+
     clearTracks: function() {
         $.each(this.findBlocksInside('searchfield', 'b-serp-item'), function(i, item) {
             item.remove();
         })
         return this;
     },
+
     appendTracks: function(tracks) {
         var bDashboard = this;
         $.each(tracks, function(i, track){
