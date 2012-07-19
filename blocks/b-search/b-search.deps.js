0c7b0317a944079595298e33e26615595dfea40e
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-19 10:07:13 +0400

    Use b-form-input for a search form

--- blocks/b-search/b-search.deps.js
+++ blocks/b-search/b-search.deps.js
@@ -1,5 +1,12 @@
 ({
     mustDeps: [
         { block: 'i-soundcloud' }
+    ],
+    shouldDeps:[
+        {
+            block: 'b-form-input',
+            mods: { theme: 'grey', size: 's', type: 'textarea' },
+            elems: ['input']
+        }
     ]
 })
