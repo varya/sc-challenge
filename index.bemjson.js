69e135a1d590717a16bfad3277849165e17d6995
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-26 16:42:55 +0400

    Use samurai buttons

--- index.bemjson.js
+++ index.bemjson.js
@@ -4,6 +4,7 @@
     js: true,
     head: [
         { elem: 'css', url: '_index.css'},
+        { elem: 'css', url: 'http://lab.simurai.com/buttons/buttons.css' },
         { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
         { block: 'i-jquery', elem: 'core' },
         { elem: 'js', url: 'http://connect.soundcloud.com/sdk.js' },
