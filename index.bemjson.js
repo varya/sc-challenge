05cce494f67995891a5c57f87807444d980e2a32
Varvara Stepanova <toivonen@yandex-team.ru>
2012-07-27 00:03:20 +0400

    Prevent caching with url param

--- index.bemjson.js
+++ index.bemjson.js
@@ -3,13 +3,13 @@
     title: 'Awesome playlists of SoundCloud',
     js: true,
     head: [
-        { elem: 'css', url: '_index.css'},
+        { elem: 'css', url: '_index.css?v=1'},
         { elem: 'css', url: 'http://lab.simurai.com/buttons/buttons.css' },
-        { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
+        { elem: 'css', url: '_index.ie.css?v=1', ie: 'lt IE 8' },
         { block: 'i-jquery', elem: 'core' },
         { elem: 'js', url: 'http://connect.soundcloud.com/sdk.js' },
-        { elem: 'js', url: 'index.bemhtml.js' },
-        { elem: 'js', url: 'index.js' }
+        { elem: 'js', url: 'index.bemhtml.js?v=1' },
+        { elem: 'js', url: 'index.js?v=1' }
     ],
     content: ''
 })
